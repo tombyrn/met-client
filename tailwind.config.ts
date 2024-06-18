7d4b24f4a832385963e01f349f6317d184de4caf
tombyrn <thomasbyrnes02@gmail.com>
2024-06-17 23:59:10 -0400

    added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,10 @@ const config: object = withMT({
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: ["Times New Roman", "sans-serif"],
+      serif: ["Times New Roman", "sans-serif"],
+      body: ["Times New Roman", "sans-serif"],
     },
   },
   plugins: [],
